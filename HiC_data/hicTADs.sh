202d628e453821e7ac5e2eb51e1bef92e34fd1ea
jb621-star <65730803+jb621-star@users.noreply.github.com>
2022-08-09 10:55:55 -0400

    Edited TAD-finding script for thesholds

--- HiC_data/hicTADs.sh
+++ HiC_data/hicTADs.sh
@@ -7,7 +7,33 @@ module load Anaconda3/2021.05
 source /hpc/group/biostat/jb621/miniconda3/etc/profile.d/conda.sh
 conda activate /hpc/group/wraylab/jb621/envs/HiCExplorer
 
-#hicFindTADs -m hicSRR1658693_corrected.h5 --outPrefix hicSRR1658693_corrected --correctForMultipleTesting fdr --numberOfProcessors 16
+hicFindTADs -m K562_3reps_corrected.h5 \
+ --outPrefix K562_3reps_TAD_step1500_min3000_max31500_thres0.05_delta0.01_fdr \
+ --thresholdComparisons 0.05 \
+ --delta 0.01 \
+ --correctForMultipleTesting fdr \
+ -p 64 \
 
-hicPlotTADs --tracks hicSRR1658693_track.ini --region chr14:20000000-90000000 \
--t 'SRR1658693 TADs on chr14' -o SRR1658693_tads.pdf
+hicFindTADs --matrix K562_3reps_corrected.h5 \
+ --outPrefix K562_3reps_TAD_step1500_min10000_max40000_thres0.01_delta0.01_fdr \
+ --thresholdComparisons 0.01 \
+ --delta 0.01 \
+ --correctForMultipleTesting fdr \
+ -p 64 \
+
+hicFindTADs --matrix K562_3reps_corrected.h5 \
+ --outPrefix K562_3reps_TAD_step1500_min10000_max40000_thres0.005_delta0.01_fdr \
+ --thresholdComparisons 0.005 \
+ --delta 0.01 \
+ --correctForMultipleTesting fdr \
+ -p 64 \
+
+hicFindTADs --matrix K562_3reps_corrected.h5 \
+ --outPrefix K562_3reps_TAD_step1500_min10000_max40000_thres0.001_delta0.01_fdr \
+ --thresholdComparisons 0.001 \
+ --delta 0.01 \
+ --correctForMultipleTesting fdr \
+ -p 64 \
+
+hicPlotTADs --tracks K562_3reps_track.ini --region chr14:20000000-40000000 \
+-t 'K562 3 replicates TADs on chr14' -o K562_3reps_tads.pdf
